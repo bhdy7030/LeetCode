a50ff6486560a9bd185543a7a02419f7fd67825e
YDU <yuandu.cs@gmail.com>
2014-11-01 16:41:49 -0400

    3Sum two pointers python accepted

--- 3Sum/Solution.py
+++ 3Sum/Solution.py
@@ -1,22 +1,24 @@
-__author__ = 'yuandu'
-
 class Solution:
+    # @return a list of lists of length 3, [[val1,val2,val3]]
     def threeSum(self, num):
-        if not num:
-            return 0
-        Solution.ret=[]
         num.sort()
-        self.DFS(num,0,0,[],0)
-        return Solution.ret
+        res = []
+        for i in range(len(num)-2):
+            if i == 0 or num[i] > num[i-1]:
+                left = i + 1; right = len(num) - 1
+                while left < right:
+                    if num[left] + num[right] == -num[i]:
+                        res.append([num[i], num[left], num[right]])
+                        left += 1; right -= 1
+                        while left < right and num[left] == num[left-1]: left +=1
+                        while left < right and num[right] == num[right+1]: right -= 1
+                    elif num[left] + num[right] < -num[i]:
+                        while left < right:
+                            left += 1
+                            if num[left] > num[left-1]: break
+                    else:
+                        while left < right:
+                            right -= 1
+                            if num[right] < num[right+1]: break
+        return res
 
-    def DFS(self,num,start,step,templist,target):
-        if step==3:
-            if target==0 and sorted(templist) not in Solution.ret:
-                Solution.ret.append(templist)
-            return
-        for i in xrange(start,len(num)):
-            if target<num[i]:
-                return
-            newnum = num[:]
-            newnum.pop(i)
-            self.DFS(newnum,start+1,step+1,templist+[num[i]],target-num[i])
